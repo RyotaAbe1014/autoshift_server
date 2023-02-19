995808a82329192b123c8b2f41170c973f79943d
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-02-19 23:08:22 +0900

    add: テーブル追加

--- auth/auth.py
+++ auth/auth.py
@@ -3,7 +3,7 @@ from fastapi.param_functions import Depends
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm.session import Session
 from db import get_db
-from models.user import User
+from models.organization import Organization
 from .hash import Hash
 from auth import oauth2
 
@@ -11,25 +11,24 @@ router = APIRouter(
     tags=['authentication']
 )
 
-
 @router.post('/token')
 def get_token(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(User).filter(
-        User.username == request.username).first()
-    if not user:
+    organization = db.query(Organization).filter(
+        Organization.name == request.username).first()
+    if not organization:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='Invalid credentials'
         )
-    if not Hash.verify_password(user.password, request.password):
+    if not Hash.verify_password(organization.password, request.password):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='Incorrect password'
         )
-    access_token = oauth2.create_access_token(data={'sub': user.username})
+    access_token = oauth2.create_access_token(data={'sub': organization.name})
     return {
         'access_token': access_token,
         'token_type': 'bearer',
-        'user_id': user.id,
-        'username': user.username
+        'organization_id': organization.id,
+        'organization_name': organization.name
     }
--- models/organization.py
+++ models/organization.py
@@ -1,5 +1,5 @@
 from db import Base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 import datetime
 
@@ -17,3 +17,44 @@ class Organization(Base):
     updated_at = Column(
         DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
     users = relationship("User", back_populates="organization")
+    business_hours = relationship(
+        "OrganizationBusinessHours", back_populates="organization")
+    exception_days = relationship(
+        "OrganizationExceptionDays", back_populates="organization")
+
+
+class OrganizationBusinessHours(Base):
+    """
+    企業の営業時間テーブル
+    """
+    __tablename__ = 'organization_business_hours'
+
+    id = Column(Integer, primary_key=True,  autoincrement=True)
+    organization_id = Column(Integer, ForeignKey(
+        'organizations.id', ondelete='CASCADE'), nullable=False)
+    day_of_week = Column(Integer, nullable=False)
+    start_time = Column(String(255), nullable=False)
+    end_time = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(
+        DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+    organization = relationship(
+        "Organization", back_populates="business_hours")
+
+
+class OrganizationExceptionDays(Base):
+    """
+    企業の例外日テーブル
+    """
+    __tablename__ = 'organization_exception_days'
+
+    id = Column(Integer, primary_key=True,  autoincrement=True)
+    organization_id = Column(Integer, ForeignKey(
+        'organizations.id', ondelete='CASCADE'), nullable=False)
+    date = Column(DateTime, nullable=False)
+    exception_type = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.now)
+    updated_at = Column(
+        DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+    organization = relationship(
+        "Organization", back_populates="exception_days")
